1be7ac8786194e42074e95cb027d4d7a0afc0a76
Guigui0025 <144000296+Guigui0025@users.noreply.github.com>
2023-10-02 12:09:26 +0200

    1 file modified

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        personne: resolve(__dirname, 'personne/index.html'),
+        article: resolve(__dirname, 'article/index.html'),
       },
     },
   },
